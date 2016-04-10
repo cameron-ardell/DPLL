33d7325a207b9f52f2836f0b5e9570457c2e9224
S. Ardell <sardell281995@gmail.com>
2016-04-10 10:16:54 -0400

    Changed how blocks are tracked, included results
    Changed how blocks are tracked so now instead of having a block array that is actively updated every time something changes, when selecting the next variable, the code first checks for unit clauses and if one is not found, it naively chooses the next available literal. It then first tries to see if there are any pure variables within the block of this literal, and if not, applies whatever splitting heuristic is selected to see if can find a better variable in the block than what the naive search came up with. All small tests work now without bugs for all heuristics applied so far, but there seems to be a bug for all large files the code is tested on for the split based on how many active variables there are for both signed and unsigned sums.

---
+++ Optimization_Spring_2016_Project_1B.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package optimization_spring_2016_project_1b;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ *
+ * @author sardell
+ */
+public class Optimization_Spring_2016_Project_1B {
+
+    private final String fileType;
+    private final String algoType;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //format of test problem input:
+        //filename, unit clauses, pure variables, branching method          
+       
+       String[] fileNames = {"small1", "small2", "small3",
+           "small4","small5", "er1", "er2", "er3",
+           "erer1", "erer2", "erer3", "ns1"};
+       
+       String[] algTypes = {"UCP", "PVE", "UCP_PVE", "max_active_clauses",
+           "max_signed_active_clauses", "weighted_unit", "log_weighted_unit"};
+       
+       //System.out.println("created arrays");
+       
+        //creates a PrintWriter which will print data to a csv file as we run tests
+        try (PrintWriter pw = new PrintWriter(new FileOutputStream(new File("DPLL.csv"), true))) {
+
+        pw.println(" " + ", " + "seconds taken, UCPS, PVEs, VS, % of VS Possible, Difference from Solution" );
+        
+        //System.out.println("wrote in title");
+        
+        //For each of 72 cases, label the data and create test instance of DPLL
+        for (int i = 0; i < fileNames.length; i++) {
+            pw.println("\n" + fileNames[i]);
+            for (int j = 0; j < 5; j++) {
+                pw.print(algTypes[j] + ", ");
+                    Optimization_Spring_2016_Project_1B prob = new Optimization_Spring_2016_Project_1B(fileNames[i], algTypes[j], pw);
+                 
+                //pw.println(" ");
+            }
+        }
+ 
+
+        } catch (IOException ioe) {
+        //If something goes wrong, throw exception
+        System.err.println("IOException: " + ioe.getMessage());
+        }                        
+    }
+    
+     //Runs a PSO test
+    public Optimization_Spring_2016_Project_1B(String fileName, String  algType, PrintWriter pw) {
+        this.fileType = fileName + ".ssat";
+        this.algoType = algType;
+        
+        int unitProp = 1;
+        int pureVar = 1;
+        String split = "naive";
+        
+        switch(this.algoType){
+            case "UCP":
+                pureVar = 0;
+                break;
+            case "PVE":
+                unitProp = 0;
+                break;
+            case "weighted_unit":
+                split = "WU";
+                break;
+            case "log_weighted_unit":
+                split = "WLU";
+                break;
+            case "max_active_clauses":
+                split = "MAC";
+                break;
+            case "max_signed_active_clauses":
+                split = "MSAC";
+            default:
+                break;
+        }
+        
+            
+        testProblem test = new testProblem(fileType,unitProp, pureVar, split);
+        //after each run, retrieve the data, so we can print it
+
+        pw.print(test.timeTaken + ", " + test.unitProps + ", " + test.PVEs + ", "
+            + test.numSplits + ", " + test.percentSplits +", " + test.matchSolution);
+        
+        pw.println(" ");
+
+    
+    }
+}
